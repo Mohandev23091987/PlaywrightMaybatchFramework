229089f8b9cbfb9c89af9c977e7f687c5b04d71f
Mohan Dev <mohandev23091987@gmail.com>
2026-08-20 09:59:07 +0530

    commit the changes

--- tests/products.spec.ts
+++ tests/products.spec.ts
@@ -5,23 +5,16 @@ import { ExcelReader } from '../utils/excelReader'
 //let productcases = ExcelReader.read('Products')
 
 // console.log(JSON.stringify(productcases))
-// console.log(ExcelReader.read('Products','SauceDemoTestData.xlsx'))
-
+console.log(ExcelReader.read('Products','SauceDemoTestData.xlsx'))
 console.log('=================================')
-
-console.log(JSON.stringify(ExcelReader.getRowByTestCaseId('Products','PROD-01')))
+console.log(ExcelReader.getRowByTestCaseId<any>('Products','PROD-01'));
+console.log((ExcelReader.getRowByTestCaseId<any>('Products','PROD-01'))?.ProductsCount)
+console.log(JSON.stringify(ExcelReader.getCellValue('Products','PROD-01','ProductsCount')))
 
 
 test.describe('SauceDemo Product Scenarios',()=>{
-
-
     test('PROD-01 - Verify products page is display and products count', async ({productsPage})=>{
-
     await productsPage.verifyPageLoaded();
-     
-      
-
-
     })
 
   
